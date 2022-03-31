b84d02b29b8e4b0a751b0f26e8f3bfc69b9f5d87
bipinkalra-scaler <bipin.kalra@scaler.com>
2022-03-31 20:56:49 +0530

    dark theme added

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -3,6 +3,7 @@ hamburgerNavbar = document.querySelector(".hamburger-navbar");
 hamburgerActiveOverlay = document.querySelector(".hamburger-active-overlay");
 
 hamburgerToggleButton = document.querySelector(".main-navbar__hamburger-icon");
+nightModeToggleButton = document.querySelector(".dark-mode-toggle");
 
 hamburgerToggleButton.addEventListener("click", (e) => {
   if (hamburgerNavbar.classList.contains("hamburger-navbar--hidden")) {
@@ -11,5 +12,14 @@ hamburgerToggleButton.addEventListener("click", (e) => {
   } else {
     hamburgerNavbar.classList.add("hamburger-navbar--hidden");
     hamburgerActiveOverlay.classList.remove("hamburger-active-overlay--active");
+  }
+});
+nightModeToggleButton.addEventListener("click", (e) => {
+  if (document.body.classList.contains("dark-theme")) {
+    document.body.classList.remove("dark-theme");
+    nightModeToggleButton.firstChild.nodeValue = "mode_night";
+  } else {
+    document.body.classList.add("dark-theme");
+    nightModeToggleButton.firstChild.nodeValue = "light_mode";
   }
 })
