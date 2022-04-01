655b33006a22f923e861c57c23c94abd840153d4
Shubham Anand <shubham@interviewbit.com>
2022-04-01 09:58:42 +0530

    add popovers

---
+++ scripts/popover.js
@@ -0,0 +1,32 @@
+const tooltips = document.querySelectorAll('.oswe-tooltip');
+
+const showEvents = ['mouseenter', 'focus'];
+const hideEvents = ['mouseleave', 'blur'];
+
+
+tooltips.forEach((tooltip) => {
+  const { parentid } = tooltip.dataset;
+  console.log(tooltip.dataset);
+  const parentEl = document.getElementById(parentid);
+  const popperInstance = Popper.createPopper(parentEl, tooltip, {
+    placement: 'top',
+    modifiers: [{ name: 'offset', options: { offset: [0, 5] } }],
+  });
+
+  const show = () => {
+    tooltip.setAttribute('data-show', '');
+    popperInstance.update();
+  }
+  
+  const hide = () => {
+    tooltip.removeAttribute('data-show');
+  }
+  
+  showEvents.forEach((event) => {
+    parentEl.addEventListener(event, show);
+  });
+  
+  hideEvents.forEach((event) => {
+    parentEl.addEventListener(event, hide);
+  });
+});
